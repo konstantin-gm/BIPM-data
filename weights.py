af4cebe9f0089f857ca9b1fd8e568c10d9f3e9eb
konstantin-gm <konstantin.gm@gmail.com>
2022-10-02 18:32:30 +0300

    for merge

--- weights.py
+++ weights.py
@@ -83,7 +83,7 @@ def fproc(fname):
         for i in range(end_ind, num_of_lines):
             ind_list.append(i)    
     
-        df = pd.read_csv(fname, sep=' ', index_col=False, skip_blank_lines=True, skipinitialspace=True, skiprows=ind_list, 
+        df = pd.read_csv(fname, sep=' ', index_col=False, skip_blank_lines=True, skipinitialspace=True, skiprows=ind_list, on_bad_lines='skip',
                      na_values=['*****', '*********'], names=['lab', 'type', 'code', mjds[2], mjds[3], mjds[4], mjds[5], mjds[6], mjds[7], 'x']).fillna(0)
         df = df.drop('x', axis=1)
         df = df[df.lab != 0]
@@ -93,80 +93,55 @@ def fproc(fname):
         print(e)
     return df
 
-def compareMasers(df):
+def compareMasers(df, listOfModels, modelNames):
     header = list(df)
     df['model'] = df['code'].apply(lambda x: str(x)[2:4] if str(x)[0:2]=="41" else 0)
     df['model'] = df['model'].astype(int)
     mjds = header[-12*2:-1]
-    mhm2010 = []
-    mhm2020 = []
-    vch1003m = []
-    vch1008 = []
-    codes_mhm2010 = []
-    codes_mhm2020 = []
-    codes_vch1003m = []
-    codes_vch1008 = []
-    num_mhm2010 = []
-    num_mhm2020 = []
-    num_vch1003m = []
-    num_vch1008 = []
-    for mjd in mjds:    
-        tmp = df[(df['model'] == 20) & (df[mjd] != 0)]
-        codes_mhm2010.extend(list(tmp['code']))    
-        #mhm2010.append(np.mean(tmp[mjd]))
-        mhm2010.append(np.std(tmp[mjd]))
-        #mhm2010.append(np.mean(np.abs(tmp[mjd])))
-        num_mhm2010.append(len(tmp[mjd]))
+    prod_for_models = []
+    codes_for_models = []    
+    num_for_models = []
     
-        tmp = df[(df['model'] == 21) & (df[mjd] != 0)]
-        codes_mhm2020.extend(list(tmp['code']))    
-        #mhm2020.append(np.mean(tmp[mjd]))
-        mhm2020.append(np.std(tmp[mjd]))
-        #mhm2020.append(np.mean(np.abs(tmp[mjd])))
-        num_mhm2020.append(len(tmp[mjd]))
-        
-    
-        tmp = df[(df['model'] == 53) & (df[mjd] != 0) & (np.abs(df[mjd]) < 100)]    
-        codes_vch1003m.extend(list(tmp['code']))    
-        #vch1003m.append(np.mean(tmp[mjd]))    
-        vch1003m.append(np.std(tmp[mjd]))
-        #vch1003m.append(np.mean(np.abs(tmp[mjd])))
-        num_vch1003m.append(len(tmp[mjd]))
-    
-        tmp = df[(df['model'] == 52) & (df[mjd] != 0)]
-        codes_vch1008.extend(list(tmp['code']))    
-        #vch1008.append(np.mean(tmp[mjd]))
-        vch1008.append(np.std(tmp[mjd]))
-        #vch1008.append(np.mean(np.abs(tmp[mjd])))
-        num_vch1008.append(len(tmp[mjd]))
+    for clck_model in listOfModels:
+        codes = []
+        prod = []
+        num = []        
+        for mjd in mjds:
+            tmp = df[(df['model'] == clck_model) & (df[mjd] != 0) & (np.abs(df[mjd]) < 100)]
+            codes.extend(list(tmp['code']))
+            prod.append(np.mean(tmp[mjd]))
+            #mhm2010.append(np.std(tmp[mjd]))
+            #mhm2010.append(np.mean(np.abs(tmp[mjd])))
+            num.append(len(tmp[mjd]))
+        codes_for_models.append(codes)
+        prod_for_models.append(prod)
+        num_for_models.append(num)            
 
-    print('Codes of MHM2010:', set(codes_mhm2010))
-    print('Codes of MHM2020:', set(codes_mhm2020))
-    print('Codes of VCH1003M:', set(codes_vch1003m))
-    print('Codes of VCH1008:', set(codes_vch1008))
-    print(vch1003m)
     plt.figure(1)
-    #plt.title('Aveage clock weights in TAI, %')
-    plt.title('Mean absolute value of clock drifts, ns/day/30days')    
-    plt.plot(mjds, mhm2010, mjds, mhm2020, mjds, vch1003m, mjds, vch1008)
+    plt.title('Aveage clock weights in TAI, %')
+    #plt.title('Mean absolute value of clock drifts, ns/day/30days')    
+    for i in range(len(listOfModels)):
+        plt.plot(mjds, prod_for_models[i])
     plt.xticks(rotation=45)
-    plt.legend(['MHM2010', 'MHM2020', 'VCH1003M', 'VCH1008 - passive H-maser'])
+    plt.legend(modelNames)
     plt.figure(2)
     plt.title('Number of clocks participating in TAI')
-    plt.plot(mjds, num_mhm2010, mjds, num_mhm2020, mjds, num_vch1003m, mjds, num_vch1008)
-    plt.legend(['MHM2010', 'MHM2020', 'VCH1003M', 'VCH1008 - passive H-maser'])
+    for i in range(len(listOfModels)):
+        plt.plot(mjds, num_for_models[i])
+    plt.legend(modelNames)
     plt.xticks(rotation=45)
     
+    return prod_for_models, codes_for_models, num_for_models, mjds    
+    
 
-dir_path = 'drifts/'
-key = 'd'
-#dir_path = 'weights/'
-#key = 'w'
+#dir_path = 'drifts/'
+#key = 'd'
+dir_path = 'weights/'
+key = 'w'
 if not os.path.exists(dir_path):
     os.makedirs(dir_path)
     
-#files = copyfromftp('d', dir_path)
-
+#files = copyfromftp(key, dir_path)
 
 files = []
 for path in os.listdir(dir_path):
@@ -191,8 +166,11 @@ for fname in ffiles:
         df1 = fproc(dir_path+fname)
         df = df.merge(df1, how='outer', on=['code', 'lab', 'type']).fillna(0)
 
-compareMasers(df)
-# header = list(df)
+listOfModels = [20, 21, 53, 52]
+namesOfModels = ['MHM2010', 'MHM2020', 'VCH1003M', 'VCH1008 - passive H-maser']
+compareMasers(df, listOfModels, namesOfModels)
+#header = list(df)
+
 # mjds = header[-12*22:-1]
 
 # mean_cs = []
@@ -217,3 +195,4 @@ compareMasers(df)
 # axes.xaxis.set_major_locator(MaxNLocator(10)) 
 # plt.plot(mjds, num_cs, mjds, num_h)
 # plt.locator_params(nbins=10)
+
